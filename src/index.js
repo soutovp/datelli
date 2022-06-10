d30336816cbe83f07e1590194dfc530371ba58d6
Mahdasah <headbanger.87@hotmail.com>
2022-06-10 01:04:45 -0300

    qerg

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import Recepcao from './js/Recepcao'
+import Conteudo from './Conteudo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Recepcao />
+    <Conteudo />
   </React.StrictMode>
 );
